81268df96809ae4cbd89a9341a85866fd42cb93f
VictorFlorez <victor_florez82171@elpoli.edu.co>
2020-09-16 14:41:16 -0500

    Se agregaron mas funciones

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ addEventListener();
 
 function addEventListener(){
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto );
 }
 
 function preguntarPresupuesto(){
@@ -18,9 +20,22 @@ function preguntarPresupuesto(){
         window.location.reload(); // Recargue la pagina hasta que ingrese algun valor
 
     presupuesto = new Presupuesto( presupuestoUsuario );
-    console.log(presupuesto)
+    
+    ui.insertarPresupuesto( presupuesto );
 }
 
+function agregarGasto(e){
+    e.preventDefault(); // ya que es un evento submit
+
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+    
+    if(nombre === '' || cantidad === '')
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+    else if (cantidad <= 0 || isNaN(cantidad)){
+        ui.imprimirAlerta('Cantidad no valida', 'error');
+    }
+}
 
 // Clases
 
@@ -33,5 +48,35 @@ class Presupuesto{
     }
 }
 
+class UI{
+    insertarPresupuesto( cantidad ){
+        const {presupuesto, restante } = cantidad;
+
+        document.querySelector('#total').textContent = presupuesto;  
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje,tipo){
+        const divAlerta = document.createElement('div');
+        divAlerta.classList.add('text-center','alert');
+
+        if(tipo === 'error')
+            divAlerta.classList.add('alert-danger');
+        else
+            divAlerta.classList.add('alert-success');
+
+        divAlerta.textContent = mensaje;
+
+        // insertar al HTML
+
+        document.querySelector('.primario').insertBefore( divAlerta, formulario);
+
+        setTimeout(() => {
+            divAlerta.remove();
+        }, 3000);
+    }
+}
+
+const ui = new UI();
 let presupuesto;
 // Funciones
